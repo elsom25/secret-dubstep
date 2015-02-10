75e5251eafe34ffcd690e6e2d57877af5bc2ee23
Jesse McGinnis <jessieelsom@hotmail.com>
2015-02-10 15:52:40 -0500

    Better dotfile management

--- modules/people/manifests/elsom25/dotfile.pp
+++ modules/people/manifests/elsom25/dotfile.pp
@@ -1,54 +1,16 @@
 class people::elsom25::dotfile {
-  repository { "${my_sourcedir}/dotfiles":
-    source => 'elsom25/dotfiles',
+  $dotfiles = "${boxen::config::srcdir}/dotfiles"
+
+  repository { $dotfiles:
+    source   => "${::github_login}/dotfiles",
+    require  => File["${boxen::config::srcdir}"],
   }
 
-  file { "${my_homedir}/.bash_aliases":
-    ensure  => link,
-    mode    => '0644',
-    target  => "${my_sourcedir}/dotfiles/bash_aliases",
-    require => Repository["${my_sourcedir}/dotfiles"],
-  }
-
-  file { "${my_homedir}/.bash_ps1":
-    ensure  => link,
-    mode    => '0644',
-    target  => "${my_sourcedir}/dotfiles/bash_ps1",
-    require => Repository["${my_sourcedir}/dotfiles"],
-  }
-
-  file { "${my_homedir}/.gitconfig":
-    ensure  => link,
-    mode    => '0644',
-    target  => "${my_sourcedir}/dotfiles/gitconfig",
-    require => Repository["${my_sourcedir}/dotfiles"],
-  }
-
-  file { "${my_homedir}/.gitignore_global":
-    ensure  => link,
-    mode    => '0644',
-    target  => "${my_sourcedir}/dotfiles/gitignore_global",
-    require => Repository["${my_sourcedir}/dotfiles"],
-  }
-
-  file { "${my_homedir}/.profile":
-    ensure  => link,
-    mode    => '0644',
-    target  => "${my_sourcedir}/dotfiles/profile",
-    require => Repository["${my_sourcedir}/dotfiles"],
-  }
-
-  file { "${my_homedir}/.pryrc":
-    ensure  => link,
-    mode    => '0644',
-    target  => "${my_sourcedir}/dotfiles/pryrc",
-    require => Repository["${my_sourcedir}/dotfiles"],
-  }
-
-  file { "${my_homedir}/.boot2dockerscripts":
-    ensure  => link,
-    mode    => '1644',
-    target  => "${my_sourcedir}/dotfiles/boot2dockerscripts",
-    require => Repository["${my_sourcedir}/dotfiles"],
+  exec { "install dotfiles":
+    provider => shell,
+    command  => "./script/install",
+    cwd      => $dotfiles,
+    creates  => "${home}/.profile",
+    require  => Repository[$dotfiles],
   }
 }
