75175af49b9e2d99e4f7aa4949230e80251993f5
Jesse McGinnis <jessieelsom@hotmail.com>
2015-02-20 09:59:40 -0500

    Update OSX settings

--- modules/people/manifests/elsom25/osx.pp
+++ modules/people/manifests/elsom25/osx.pp
@@ -3,9 +3,25 @@ class people::elsom25::osx {
   # osx::recovery_message { 'If this Mac is found, please call (226) 868-0808': }
 
   include osx::global::expand_save_dialog
+  include osx::global::disable_remote_control_ir_receiver
+  include osx::global::tap_to_click
+  include osx::global::natural_mouse_scrolling
+
+  include osx::dock::disable_dashboard
+  include osx::dock::dim_hidden_apps
+  class { 'osx::dock::icon_size':
+    size => 46
+  }
+  class { 'osx::dock::magnification':
+    magnification => true,
+    magnification_size => 65
+  }
+  class { 'osx::dock::position':
+    position => 'bottom'
+  }
 
   include osx::finder::empty_trash_securely
-  include osx::finder::show_hidden_files
+  # include osx::finder::show_hidden_files
   include osx::finder::enable_quicklook_text_selection
 
   include osx::safari::enable_developer_mode
