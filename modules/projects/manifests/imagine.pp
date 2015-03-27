0ca4032f111869ce67deafec674c2eaac0f43028
Jesse McGinnis <jessieelsom@hotmail.com>
2015-03-27 11:08:25 -0400

    New projects. New Docker. Update imagine node version.

--- modules/projects/manifests/imagine.pp
+++ modules/projects/manifests/imagine.pp
@@ -1,5 +1,5 @@
 class projects::imagine {
-  $node_version = "v0.12.0"
+  $node_version = "v0.12.1"
   include phantomjs
 
   boxen::project { "imagine":
@@ -10,14 +10,6 @@ class projects::imagine {
   }
 
   nodejs::module { 'grunt-cli':
-    node_version  => $node_version
-  }
-
-  nodejs::module { 'police':
-    node_version  => $node_version
-  }
-
-  nodejs::module { 'constable':
     node_version  => $node_version
   }
 }
---
+++ modules/projects/manifests/postal-when.pp
@@ -0,0 +1,11 @@
+class projects::postal-when {
+  $node_version = "v0.12.0"
+  include phantomjs
+
+  boxen::project { "postal-when":
+    nodejs        => $node_version,
+
+    source        => "elsom25/postal.when",
+    dir           => "${boxen::config::srcdir}/forks/postal.when"
+  }
+}
