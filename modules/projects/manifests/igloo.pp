68c579b6f386791b8cd52835bb379b2a7388ef19
Jesse McGinnis <jessieelsom@hotmail.com>
2015-08-25 10:51:17 -0400

    Add ruby to igloo

--- modules/projects/manifests/igloo.pp
+++ modules/projects/manifests/igloo.pp
@@ -1,8 +1,10 @@
 class projects::igloo {
+  $ruby_version = "2.2.2"
   $node_version = "0.12.4"
   include phantomjs
 
   boxen::project { "igloo":
+    ruby          => $ruby_version,
     nodejs        => $node_version,
 
     source        => "Boltmade/igloo",
