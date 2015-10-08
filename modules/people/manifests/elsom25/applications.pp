74ce4922e7698be01320ef033ed9fc4b17cf398e
Jesse McGinnis <jesse@jcmcginnis.com>
2015-10-08 16:53:57 -0400

    More cask goodness. nginx is still broken, looks like fix is oncoming however.

--- modules/people/manifests/elsom25/applications.pp
+++ modules/people/manifests/elsom25/applications.pp
@@ -6,38 +6,46 @@ class people::elsom25::applications {
 
   # Install these with Cask - http://caskroom.io
   $cask_packages = [
-    'caffeine',
-    'dropbox',
-    'evernote',
-    'firefox',
-    'flux',
-    'github',
-    'google-hangouts',
-    'google-chrome',
-    'slack',
-    'iterm2',
-    'sublime-text',
-    'java',
-    'vagrant',
-    'virtualbox',
+    "1password",
+    "alfred",
+    "bettertouchtool",
+    "caffeine",
+    "crashplan",      # TODO: Not installed via boxen yet...
+    "dropbox",
+    "evernote",
+    "firefox",
+    "flux",
+    "goofy",
+    "google-chrome",
+    "google-drive",
+    "iterm2",
+    "java",           # TODO: Not installed via boxen yet...
+    "path-finder",
+    "slack",
+    "sublime-text",
+    "todoist",
+    "vagrant",        # TODO: Not installed via boxen yet...
+    "virtualbox",     # TODO: Not installed via boxen yet...
+    "ynab",
   ]
 
   # Install the cask packages.
   # You might need some sort of sudo hack. Tries to symlink stuff to non-writable dir
+  # TODO: sudo hack not working yet...
   package { $cask_packages:
     ensure   => present,
-    provider => 'brewcask',
-    require  => Sudoers['boxen'],
+    provider => "brewcask",
+    require  => Sudoers["boxen"],
   }
 
   # Workaround for puppet-brewcask
   # https://github.com/phinze/puppet-brewcask/issues/9
   # https://github.com/caskroom/homebrew-cask/issues/7750
-  # Let the 'staff' group write to this.
-  file { ['/usr/local', '/usr/local/bin']:
-    ensure   => 'directory',
-    owner    => 'root',
-    group    => 'staff',
-    mode     => '0775',
+  # Let the "staff" group write to this.
+  file { ["/usr/local", "/usr/local/bin"]:
+    ensure   => "directory",
+    owner    => "root",
+    group    => "staff",
+    mode     => "0775",
   }
 }
