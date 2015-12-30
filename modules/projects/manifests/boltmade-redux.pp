4ab756937074aaeca15beb00fe4dde1839019fb6
Jesse McGinnis <jesse@jcmcginnis.com>
2015-12-29 22:19:45 -0500

    Remove old projects, update boltmade.com project.

--- modules/projects/manifests/boltmade-redux.pp
+++ modules/projects/manifests/boltmade-redux.pp
@@ -3,11 +3,9 @@ class projects::boltmade-redux {
   include phantomjs
   phantomjs::version { "2.0.0": }
   $ruby_version = "2.2.2"
-  $node_version = "0.12.0" # for bower dependencies
 
   boxen::project { "boltmade-redux":
     ruby          => $ruby_version,
-    nodejs        => $node_version,
 
     source        => "boltmade/boltmade.com",
     dir           => "${boxen::config::srcdir}/work/boltmade.com-redux"
--- modules/projects/manifests/imagine-configurator.pp
+++
@@ -1,6 +0,0 @@
-class projects::imagine-configurator {
-  boxen::project { "imagine-configurator":
-    source        => "boltmade/imagine-configurator",
-    dir           => "${boxen::config::srcdir}/work/imagine-configurator"
-  }
-}
